f552f83b87e1103c99a9beea89e06549e3fcc484
Bullets-jyr <jyr@bullets.co.kr>
2023-04-16 11:42:54 +0900

    Initial commit

---
+++ bin/datetime_and_duration.dart
@@ -0,0 +1,5 @@
+import 'package:datetime_and_duration/datetime_and_duration.dart' as datetime_and_duration;
+
+void main(List<String> arguments) {
+  print('Hello world: ${datetime_and_duration.calculate()}!');
+}
---
+++ test/datetime_and_duration_test.dart
@@ -0,0 +1,8 @@
+import 'package:datetime_and_duration/datetime_and_duration.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
