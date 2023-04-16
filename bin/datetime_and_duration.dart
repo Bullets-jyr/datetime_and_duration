54189744d1118a5f8ec62e4b9f016a4d6eae5df7
Bullets-jyr <jyr@bullets.co.kr>
2023-04-16 12:09:56 +0900

    [이론] DateTime 및 Duration

--- bin/datetime_and_duration.dart
+++ bin/datetime_and_duration.dart
@@ -1,5 +1,48 @@
 import 'package:datetime_and_duration/datetime_and_duration.dart' as datetime_and_duration;
 
 void main(List<String> arguments) {
-  print('Hello world: ${datetime_and_duration.calculate()}!');
+  // 날짜
+  DateTime now = DateTime.now();
+
+  print(now);
+  print(now.year);
+  print(now.month);
+  print(now.day);
+  print(now.hour);
+  print(now.minute);
+  print(now.second);
+  print(now.millisecond);
+
+  // 기간
+  Duration duration = Duration(seconds: 60);
+
+  print(duration);
+  print(duration.inDays);
+  print(duration.inHours);
+  print(duration.inMinutes);
+  print(duration.inSeconds);
+  print(duration.inMilliseconds);
+
+  DateTime specificDay = DateTime(
+    2017,
+    11,
+    23,
+  );
+
+  print(specificDay);
+
+  final difference = now.difference(specificDay);
+
+  print(difference);
+  print(difference.inDays);
+  print(difference.inHours);
+  print(difference.inMinutes);
+
+  print(now.isAfter(specificDay));
+  print(now.isBefore(specificDay));
+
+  print('-----------------------');
+  print(now);
+  print(now.add(Duration(hours: 10)));
+  print(now.subtract(Duration(seconds: 20)));
 }
